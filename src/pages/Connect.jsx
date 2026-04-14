9da7a0d39ae44f1f58067948f26821ec2b0eb91d
ahmed <ahmed.codex1@gmail.com>
2026-04-14 20:37:11 +0200

    update connect

--- src/pages/Connect.jsx
+++ src/pages/Connect.jsx
@@ -41,7 +41,7 @@ const Connect = () => {
           <div className='flex flex-col md:flex-row items-center justify-center gap-8  '>
               <div className="connect flex flex-col items-center justify-between bg-black/20 w-60 md:w-50 h-90 p-8 shadow-[0_0_15px_rgba(59,100,150,0.7)] text-gray-500 rounded-lg">
                  {info.map((item , index)=>(
-                   <a href={item.href} target='_blank' key={index} className='cursor-pointer hover:text-gray-300 hover:shadow-[0_0_15px_rgba(59,100,150,0.7)]  w-40 transition-all duration-300  flex items-center justify-center gap-5 py-3 text-lg bg-black/15 rounded-4xl border-gray-600 last:border-none last:mb-0'>
+                   <a href={item.href} target='_blank' key={index} className='cursor-pointer hover:text-gray-300 hover:shadow-[0_0_15px_rgba(59,100,150,0.7)]  w-40 transition-all duration-300  flex items-center justify-center gap-5 py-3 text-lg bg-black/15 rounded-4xl  last:mb-0'>
                         <item.icon className='size-6' />
                         <p>{item.name}</p>
                    </a>
@@ -51,11 +51,11 @@ const Connect = () => {
                   <h2 className='text-white text-xl md:text-2xl font-serif capitalize tracking-wide flex items-center justify-center mb-4'>Send me a message </h2>
                   <form ref={form} onSubmit={sendEmail} className='flex items-start justify-center text-white flex-col border-t pt-3 border-gray-300'>
                     <label className='mb-2'>Name</label>
-                    <input type="text" name="name" required className='bg-black/20 py-2 px-2 mb-4 w-55 sm:w-75 md:w-85 rounded-lg text-gray-500 focus:shadow-[0_0_20px_rgba(59,130,246,0.7)]' placeholder='Full Name '/>
+                    <input type="text" name="name" required className='bg-black/20 py-2 px-2 mb-4 w-55 sm:w-75 md:w-85 rounded-lg text-gray-500 focus:shadow-[0_0_20px_rgba(59,130,246,0.7)] focus:border-none' placeholder='Full Name '/>
                     <label className='mb-2'>Email</label>
-                    <input type="email" name="email" required className='bg-black/20 py-2 px-2 mb-4 w-55 sm:w-75 md:w-85 rounded-lg text-gray-500 focus:shadow-[0_0_20px_rgba(59,130,246,0.7)]' placeholder='E-Mail ' />
+                    <input type="email" name="email" required className='bg-black/20 py-2 px-2 mb-4 w-55 sm:w-75 md:w-85 rounded-lg text-gray-500 focus:shadow-[0_0_20px_rgba(59,130,246,0.7)] focus:border-none' placeholder='E-Mail ' />
                     <label className='mb-2'>Message</label>
-                    <textarea name="message" required className='bg-black/20 py-2 px-2 mb-4 w-55 sm:w-75 md:w-85 rounded-lg text-gray-500 focus:shadow-[0_0_20px_rgba(59,130,246,0.7)]' placeholder='Message ' />
+                    <textarea name="message" required className='bg-black/20 py-2 px-2 mb-4 w-55 sm:w-75 md:w-85 rounded-lg text-gray-500 focus:shadow-[0_0_20px_rgba(59,130,246,0.7)] focus:border-none' placeholder='Message ' />
                     <input type="submit" value="Send" className='w-full bg-blue-900 py-2 transition-all duration-300 rounded-2xl cursor-pointer hover:bg-indigo-800'/>
                   </form>
               </div>
